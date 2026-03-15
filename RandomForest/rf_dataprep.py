4911c91e998aa81e6a459afe7f6d6f129512791a
Stella R <stella.rovazzi@gmail.com>
2026-03-15 14:51:44 -0400

    Adding in the log mbmb

--- RandomForest/rf_dataprep.py
+++ RandomForest/rf_dataprep.py
@@ -10,6 +10,9 @@ dataset = pd.read_csv(f'{ticker}_unadjusted_prices_2007_2018.csv', skiprows=2)
 dataset.rename(columns={"Unnamed: 1": "Price"}, inplace=True)
 # print(dataset)
 
+# normalizing the data
+dataset["Price"] = np.log(dataset["Price"]) # I doubt anything is 0....
+
 # Setting up lags -  efficently so computer doesn't die
 lags = range(1,120) # will look at past 120 days
 
@@ -25,3 +28,7 @@ rf_input = dataset.dropna().reset_index(drop=True)
 
 # Writing to a CSV
 rf_input.to_csv(f'./RFInputs/{ticker}_rf_input.csv', index=False)
+
+# To verify this will give:
+#       Columns: Current date, Lag for day (for 120)
+#       Rows: The days we are looking at in total (That have lags - so our first row is only starting at 120 date)
