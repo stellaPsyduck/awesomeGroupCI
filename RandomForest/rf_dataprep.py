b6d960ecf033a35ebd7adb4237a2129f18e511a9
Stella R <stella.rovazzi@gmail.com>
2026-03-15 04:08:27 -0400

    Set up script for single dataset
    Next Steps: Create a list of all chosen companies, so we can automate it
    a bit easier

---
+++ RandomForest/rf_dataprep.py
@@ -0,0 +1,27 @@
+# imports
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+# Loading in dataset - could consider making this less annoying but testing one for now
+ticker = "BB.TO"
+dataset = pd.read_csv(f'{ticker}_unadjusted_prices_2007_2018.csv', skiprows=2)
+dataset.rename(columns={"Unnamed: 1": "Price"}, inplace=True)
+# print(dataset)
+
+# Setting up lags -  efficently so computer doesn't die
+lags = range(1,120) # will look at past 120 days
+
+lagged = pd.concat(
+    [dataset["Price"].shift(lag) for lag in lags], # create a lag number of values for every price
+    axis=1
+)
+lagged.columns = [f"lag_{lag}" for lag in lags]
+dataset = pd.concat([dataset, lagged], axis=1) # connect back to og dataset
+
+# Remove rows with missing lag values
+rf_input = dataset.dropna().reset_index(drop=True)
+
+# Writing to a CSV
+rf_input.to_csv(f'./RFInputs/{ticker}_rf_input.csv', index=False)
